14ddcb8a84a022ff94537f7fc5acae374484af9e
rani <541251070@student.smktelkom-pwt.sch.id>
2026-05-30 13:11:37 +0700

    ubah daftar login

--- masuk.php
+++ masuk.php
@@ -52,12 +52,15 @@ if (isset($_POST['login'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Masuk - Atma Java</title>
 
+    <link rel="icon" type="image/png" href="assets/img_logo/logoatma.png">s
+
     <link rel="stylesheet" href="css/masuk.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"/>
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+
 </head>
 <body>
 
