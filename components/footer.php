53a6b7cf50cd90ec66d77f0e6f4130859dde00f2
Hayasalsa12 <541251138@student.smktelkom-pwt.sch.id>
2026-06-10 20:48:13 +0700

    gajadi

--- components/footer.php
+++ components/footer.php
@@ -1,6 +1,4 @@
-//footer = bagian bawah website yang 
-berisi informasi tambahan
-<footer> 
+<footer>
     <div class="footer-left">
         <div class="footer-logo">
             <img src="assets/img_logo/logoatma.png" alt="logo">
--- components/navbar.php
+++ components/navbar.php
@@ -1,4 +1,3 @@
-// navbar = menu navigasi di bagian atas website
 <?php
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
